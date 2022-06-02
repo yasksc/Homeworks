9d0f83211ace898ae69e731eddd859fff3ea274a
Yaroslav Skovoroda <YASK@simcorp.com>
2022-06-02 14:21:16 +0200

    remove development 1 from master branch

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -11,7 +11,6 @@ namespace Homework1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("future-branch-1 - development 1");
         }
     }
 }
