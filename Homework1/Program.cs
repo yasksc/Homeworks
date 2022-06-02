426a623a70ff5479717a187b8f36a856bf7813aa
Yaroslav Skovoroda <YASK@simcorp.com>
2022-06-02 13:31:14 +0200

    add development 1 to future-branch-1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -11,7 +11,7 @@ namespace Homework1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("future-branch-1");
+            Console.WriteLine("future-branch-1 - development 1");
         }
     }
 }
